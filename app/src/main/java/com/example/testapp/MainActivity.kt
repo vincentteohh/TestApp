846e006c47a73af553ee96b4a173c17325146c55
anreh <anurehka@gmail.com>
2022-07-26 09:27:00 +0800

    complete

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -4,26 +4,26 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
-import org.w3c.dom.Text
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImg: ImageView
     lateinit var numberText: TextView
-    private lateinit var editPlayerName:TextView
-    private lateinit var playerNameTV: TextView
+    lateinit var editPlayerName: EditText
+    lateinit var playerNameTV: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         diceImg = findViewById(R.id.diceImg)
         numberText = findViewById(R.id.numberText)
+        editPlayerName = findViewById(R.id.editPlayerName)
+        playerNameTV = findViewById(R.id.playerName)
 //        val rollButton: Button = findViewById(R.id.rollButton)
         val rollButton = findViewById<Button>(R.id.rollButton)
         rollButton.setOnClickListener{ rollDice()}
-
+        val updateButton = findViewById<Button>(R.id.updatePlayerNameBtn)
+        updateButton.setOnClickListener{updatePlayerName(it)}
     }
 
     private fun rollDice(){
@@ -49,9 +49,11 @@ class MainActivity : AppCompatActivity() {
     private fun updatePlayerName(view: View){
         playerNameTV.text = editPlayerName.text
 
-        editPlayerName.setText("")
+        editPlayerName.setText(" ")
         editPlayerName.clearFocus()
-    }
 
-    val imm = getSystemService(Context.INPUT_SERVICE)
+        //Hide keyboard after update player name
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
